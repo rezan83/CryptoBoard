cca360511e4cb5134b497b70118c3f33f4ffa665
rezan83 <rezan.moh83@gmail.com>
2020-09-23 23:15:49 +0200

    Updates

---
+++ precache-manifest.64c75babcb5f1df88865d94e13a2c734.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ffacf729f195b9172a02cce7c8b13fd",
+    "url": "/CryptoBoard/index.html"
+  },
+  {
+    "revision": "3aee4edf80fdf9245e00",
+    "url": "/CryptoBoard/static/css/main.240a1125.chunk.css"
+  },
+  {
+    "revision": "9bbafd1b4f4523c900b8",
+    "url": "/CryptoBoard/static/js/2.d2011e1c.chunk.js"
+  },
+  {
+    "revision": "23ec6c7f0411a6141095a6c739b32980",
+    "url": "/CryptoBoard/static/js/2.d2011e1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce1a28cb156fc2fa1e58",
+    "url": "/CryptoBoard/static/js/3.cafceb7e.chunk.js"
+  },
+  {
+    "revision": "3aee4edf80fdf9245e00",
+    "url": "/CryptoBoard/static/js/main.eec5ce5e.chunk.js"
+  },
+  {
+    "revision": "609c21401ea77b016b66",
+    "url": "/CryptoBoard/static/js/runtime-main.4298baea.js"
+  }
+]);
